9856b0cfd4b2fc1f6e37b5401854b4b7899ba4f8
Michael Hegner <hegner123@gmail.com>
2024-07-20 12:35:44 -0400

    updates to command call

--- lua/how.lua
+++ lua/how.lua
@@ -6,8 +6,8 @@ function how.setup()
     vim.api.nvim_create_user_command("How",
          function(cmd)
             vim.echo("how")
-            return {    desc = "Echoes the word 'how' to the command bar"}
-        end
+        end,
+        {    desc = "Echoes the word 'how' to the command bar"}
     )
 
 
