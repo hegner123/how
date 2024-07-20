c20558f462129d3116e5bfb517c8a8ab2e98f0c6
Michael Hegner <hegner123@gmail.com>
2024-07-20 12:33:56 -0400

    updated

--- lua/how.lua
+++ lua/how.lua
@@ -1,34 +1,7 @@
------ Open the file in write mode
-local file = io.open("log.log", "w")
 
--- Check if the file was successfully opened
-if file then
-    -- Write a string to the file
-    file:write("This is a log message.\n")
+local how = {}
 
-    -- Close the file
-    file:close()
-
-    print("Log message written to log.log")
-else
-    print("Error opening file log.log")
-end
----@class How
-local How = {}
-
-How.__index = How
-
-
-
-local the_How = How
-
----@param self How
----@return How
-function How.setup(self)
-    if self ~= the_How then
-        ---@diagnostic disable-next-line: cast-local-type
-        self = the_How
-    end
+function how.setup()
 
     vim.api.nvim_create_user_command("How",
          function(cmd)
@@ -38,7 +11,6 @@ function How.setup(self)
     )
 
 
-    return self
 end
 
-return the_How
+return how
