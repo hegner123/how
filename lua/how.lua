b06c265ff031c197279314953465c8fa30d56cbf
Michael Hegner <hegner123@gmail.com>
2024-07-20 13:13:14 -0400

    updated

--- lua/how.lua
+++ lua/how.lua
@@ -3,7 +3,11 @@ local how = {}
 function how.setup()
     vim.api.nvim_create_user_command("How",
         function(cmd)
+            if cmd == "search" then
+                print("%s@search@replace@g")
+            else
             print("how")
+            end
         end,
         { desc = "Echoes the word 'how' to the command bar" }
     )
