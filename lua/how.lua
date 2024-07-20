7fcb12e81af70bbcbf646c71cb1e3abd16a2667a
Michael Hegner <hegner123@gmail.com>
2024-07-20 12:18:04 -0400

    updates to setup

--- lua/how.lua
+++ lua/how.lua
@@ -1,11 +1,32 @@
 -- lua/tips.lua
+--
+---@class How
+---@field config setupHow
+local How = {}
 
-local function setupHow()
-vim.api.nvim_create_user_command("How",
-  function(cmd)
-return {    desc = "Echoes the word 'how' to the command bar"}
-end
+How.__index = How
+
+
+
+local the_How = How:new()
+
+---@param self How
+---@return How
+function How.setup(self)
+    if self ~= the_How then
+        ---@diagnostic disable-next-line: cast-local-type
+        self = the_How
+    end
+
+    vim.api.nvim_create_user_command("How",
+         function(cmd)
+            vim.echo("how")
+            return {    desc = "Echoes the word 'how' to the command bar"}
+        end
     )
+
+
+    return self
 end
 
-return setupHow
+return the_How
