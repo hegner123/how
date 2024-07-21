e486f0542f7f0d2932e611f28b504023d8bc843a
Michael Hegner <hegner123@gmail.com>
2024-07-21 13:08:17 -0400

    removed luarocks paths

--- init.lua
+++ init.lua
@@ -1,5 +1,2 @@
 require("lua.how")
--- Add LuaRocks paths
-package.path = package.path .. ';' .. os.getenv("HOME") .. '/.luarocks/share/lua/5.4/?.lua'
-package.cpath = package.cpath .. ';' .. os.getenv("HOME") .. '/.luarocks/lib/lua/5.4/?.so'
 
