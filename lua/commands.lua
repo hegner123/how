4d31cd608d7cded03b8234fc69fe9fbb8d538439
Michael Hegner <hegner123@gmail.com>
2024-07-28 10:50:59 -0400

    commented out some conditionals for troubleshooting

--- lua/commands.lua
+++ lua/commands.lua
@@ -24,15 +24,15 @@ local function commands(how)
     ---------------------
     vim.api.nvim_create_user_command("How",
         function(opts)
-            if opts.count < 1 then
+           -- if opts.count < 1 then
                 local keys = get_users_keys()
                 local result = actions.tableToString(keys)
                 print(result)
-            else
-                local arg1 = opts.fargs[1]
-                local result = actions.tableToString(how.get_setting(arg1))
-                print(result)
-            end
+            --else
+             --   local arg1 = opts.fargs[1]
+             --   local result = actions.tableToString(how.get_setting(arg1))
+             --   print(result)
+            --end
         end,
         {
             nargs = "?",
