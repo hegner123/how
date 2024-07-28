98d71f53d73d52dc56dc126c3ab8197e682f3ae6
Michael Hegner <hegner123@gmail.com>
2024-07-28 09:49:35 -0400

    updates to logs

--- lua/commands.lua
+++ lua/commands.lua
@@ -27,7 +27,7 @@ local function commands(how)
 
     local function get_users_keys()
         local settings = actions.read_settings("./demo.json")
-        print(actions.tableToString(settings))
+        print(settings)
         local keys = {}
         for key, _ in pairs(settings) do
             table.insert(keys, key)
