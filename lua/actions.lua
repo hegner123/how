14de16c604ec60a31f886d3f5944c155d8772d3b
Michael Hegner <hegner123@gmail.com>
2024-07-28 09:16:33 -0400

    Added tests for development, added, table to string function, added definition crud operations

---
+++ lua/actions.lua
@@ -0,0 +1,68 @@
+local json = require("json")
+local actions = {}
+
+-- Functiion to read settings from the JSON file
+function actions.read_settings(settings_path)
+    local file = io.open(settings_path, 'r')
+    if file then
+        local content = file:read('*a')
+        file:close()
+        return json.decode(content)
+    else
+        return {}
+    end
+end
+
+-- Function to write settings to the JSON file
+function actions.write_settings(settings_path, settings)
+    local file = io.open(settings_path, 'w')
+    if file then
+        file:write(json.encode(settings))
+        file:close()
+    else
+        print('Error: Could not write to settings file')
+    end
+end
+
+
+function actions.append_definition(settings_path, key, value)
+    local settings = actions.read_settings(settings_path)
+    print(tostring(settings))
+    local definitions = settings["definitions"]
+    print(definitions)
+    definitions[key] = value
+    actions.write_settings(settings_path,settings)
+end
+
+function actions.delete_definition(path, key)
+    local settings = actions.read_settings(path)
+    local definitions = settings["definitions"]
+    if definitions[key] then
+        definitions[key] = nil;
+        actions.write_settings(path, settings)
+    else
+        print("Definition not found")
+    end
+end
+function actions.tableToString(tbl, indent)
+    indent = indent or 0
+    local result = "{\n"
+    local separator = ""
+    local indentString = string.rep("  ", indent)
+
+    for key, value in pairs(tbl) do
+        result = result .. indentString .. separator .. tostring(key) .. " = "
+        if type(value) == "table" then
+            result = result .. actions.tableToString(value, indent + 1)
+        else
+            result = result .. tostring(value)
+        end
+        separator = ",\n"
+    end
+
+    result = result .. "\n" .. string.rep("  ", indent - 1) .. "}"
+    return result
+end
+
+
+return actions
--- lua/commands.lua
+++ lua/commands.lua
@@ -1,85 +1,60 @@
 local json = require("json")
 local settings_path = vim.fn.stdpath('config') .. '/how_settings.json'
+local actions = require("actions")
 
 local function commands(how)
--- Functiion to read settings from the JSON file
-local function read_settings()
-    local file = io.open(settings_path, 'r')
-    if file then
-        local content = file:read('*a')
-        file:close()
-        return json.decode(content)
-    else
-        return {}
+    local function get_length(var)
+        local var_type = type(var)
+        if var_type == "string" then
+            return #var
+        elseif var_type == "table" then
+            local count = 0
+            for _ in pairs(var) do
+                count = count + 1
+            end
+            return count
+        else
+            return nil -- Length is not defined for other types
+        end
     end
-end
 
--- Function to write settings to the JSON file
-local function write_settings(settings)
-    local file = io.open(settings_path, 'w')
-    if file then
-        file:write(json.encode(settings))
-        file:close()
-    else
-        print('Error: Could not write to settings file')
+    -- Functiion to read settings from the JSON file
+
+    local function extract_key_value(input)
+        local key, value = input:match('"(.-)"%s+"(.-)"')
+        return { key, value }
     end
-end
 
--- Function to update a specific setting
-function how.update_setting(key, value)
-    local settings = read_settings()
-    settings[key] = value
-    write_settings(settings)
-end
-
--- Function to get a specific setting
-function how.get_setting(key)
-    local settings = read_settings()
-    return settings[key]
-end
-
--- Function to delete a specific setting
-function how.delete_setting(key)
-    local settings = read_settings()
-    if settings[key] then
-        settings[key] = nil
-        write_settings(settings)
-        print("Deleted setting: " .. key)
-    else
-        print("Setting not found: " .. key)
+    local function get_users_keys()
+        local settings = actions.read_settings()
+        local keys = {}
+        for key, _ in pairs(settings) do
+            table.insert(keys, key)
+        end
+        return keys
     end
-end
 
-local function extract_key_value(input)
-  local key, value = input:match('"(.-)"%s+"(.-)"')
-  return {key, value}
-end
-
-local function get_users_keys()
-    local settings = read_settings()
-    local keys = {}
-      for key, _ in pairs(settings) do
-        table.insert(keys, key)
-      end
-  return keys
-end
+    ---------------------
+    --  User Commands  --
+    ---------------------
     vim.api.nvim_create_user_command("How",
         function(opts)
-            local arg1 = opts.fargs[1]
-
-            if arg1 == "search" then
-                print("%s@search@replace@g")
+            if get_length(opts) < 1 then
+                local result = actions.tableToString(how.get_settings())
+                print(result)
             else
-                print(arg1)
+                local arg1 = opts.fargs[1]
+                local result = actions.tableToString(how.get_setting(arg1))
+                print(result)
             end
         end,
-         {
+        {
             nargs = "?",
-             complete = function(ArgLead, CmdLine, CursorPos)
+            complete = function(ArgLead, CmdLine, CursorPos)
                 local keys = get_users_keys()
-            print(ArgLead)
-            print(CmdLine)
-            print(CursorPos)
+                print(ArgLead)
+                print(CmdLine)
+                print(CursorPos)
                 -- return completion candidates as a list-like table
                 return keys
             end,
@@ -91,7 +66,7 @@ end
             local arg = opts.fargs[1]
             local parsed = extract_key_value(arg)
 
-            print(parsed[1],parsed[2])
+            print(parsed[1], parsed[2])
         end,
         {
             nargs = 1,
@@ -107,7 +82,7 @@ end
         {
             nargs = 1,
             desc = "Delete Settings"
-        } )
+        })
 end
 
 return commands
