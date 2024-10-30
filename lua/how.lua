e21c2dad03ba9dc024e7b7c33e3ab8305c856501
Michael Hegner <hegner123@gmail.com>
2024-10-30 17:32:27 -0400

    updates

--- lua/how.lua
+++ lua/how.lua
@@ -1,11 +1,10 @@
-
 local module = require("how.module")
 local sqlite3 = require("lsqlite3complete")
 
 ---@class Config
 ---@field opt string Your config option
 local config = {
-  opt = "Hello!",
+    opt = "Hello!",
     sqlitePath = "",
 }
 ---@class MyModule
@@ -23,11 +22,11 @@ sqlite3.open("test.db")
 -- you can define your setup function here. Usually configurations can be merged, accepting outside params and
 -- you can also put some validation here for those.
 How.setup = function(args)
-  How.config = vim.tbl_deep_extend("force", How.config, args or {})
+    How.config = vim.tbl_deep_extend("force", How.config, args or {})
 end
 
 How.hello = function()
-  return module.greet(How.config.opt)
+    return module.greet(How.config.opt)
 end
 
 return How
