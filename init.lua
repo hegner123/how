139dde6a3fe9dd4fca29c2ae7e36f7d1b8e59fb2
Michael Hegner <hegner123@gmail.com>
2024-10-30 17:31:18 -0400

    updates to init path

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
 require("lua.how")
+-- Add LuaRocks path to Lua's package path (adjust LuaRocks version if necessary)
+package.cpath = package.cpath .. "/Users/home/.luarocks/lib/lua/5.1/lsqlite3complete.so"
 
 
